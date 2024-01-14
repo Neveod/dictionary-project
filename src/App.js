96b1ce887fa8d3a6b4a07f7aba406af1e5956db7
Neveod <neve@ravenbay.com>
2024-01-14 13:24:16 -0700

    added the basic search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <main>
+          <Dictionary />
+        </main>
+      </header>
     </div>
   );
 }

89b934fd641628f0ef338d91471b1b9f47aa7d8f
Neveod <neve@ravenbay.com>
2024-01-14 17:27:46 -0700

    rendered the word meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data.meanings[0]);
+    setResults(response.data);
   }
   function search(event) {
     event.preventDefault();
@@ -22,6 +25,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }

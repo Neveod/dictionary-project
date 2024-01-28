7c1ee53ad9de13599afe4ff94daa6d9dbc7248db
Neveod <neve@ravenbay.com>
2024-01-27 21:36:26 -0700

    made the dictionary more functional

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,31 +5,47 @@ import Results from "./Results";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data);
   }
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiKey = `b5b6fea0f8dfd4f557b6o7066130170t`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     //console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  return (
-    <div className="Dictionary">
-      <section>
-        <h1>📖</h1>
-        <form onSubmit={search}>
-          <input type="search" onChange={handleKeywordChange} />
-        </form>
-        <div className="hint">suggested words: book, sunrise, help, forest</div>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>📖</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="hint">
+            suggested words: book, sunrise, help, forest
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
